b7e100d8e13e32bd6befe4a2b4631cc2869db5c8
Charlotte Gunn Hunt <charlotte.gunn@kellogg.northwestern.edu>
2023-06-04 20:46:11 +0000

    Still not showing post image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
 
+    # alternative responses to requests other than HTML
+    respond_to do |format|
+      format.html # implicitly renders posts/index.html.erb
+      format.json do
+        render :json => @posts
+      end
+    end
+  end
 
   def new
     @post = Post.new
-    @post.place_id = params["place_id"]
   end
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
@@ -14,7 +24,18 @@ class PostsController < ApplicationController
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = @current_user["id"]
     @post.uploaded_image.attach(params["post"]["uploaded_image"])
-    @post.save
+    @post.save   
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places/#{@post["place_id"]}"
+  end
+  # removes security restrictions for API calls
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
   end
 end
